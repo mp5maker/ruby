b3c26e0d9c24cbc1432173ab0a08d8d8e24c50f1
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-08-23 21:31:47 +0600

    Basic String Manipulation

---
+++ 1.basics/string.rb
@@ -0,0 +1,20 @@
+some_string = "heLl1234SDDr"
+some_value = -56.234
+
+## Object's method in Ruby is also known as receiver
+## Ruby is a Object Oriented Language
+
+## Uppercase the string
+puts(some_string.upcase)
+
+## Reverses the string
+puts(some_string.reverse)
+
+## Checks whether the number is even or not
+puts(67.even?)
+
+## Absolute value the number
+puts(some_value.abs)
+
+## Gets the class
+puts(some_string.class)
